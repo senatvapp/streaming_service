06f29fca114831af1110a78b8124f434ac715dec
AndresOrdonez369 <oswaldordonez@unicauca.edu.co>
2024-12-01 19:59:08 -0500

    Finish .env

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,23 @@
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 
-// const serviceAccount = require('../login.json');
-console.log
+// // const serviceAccount = require('../login.json');
+// console.log
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
+
+const admin = require("firebase-admin");
+
+require("dotenv").config();
+//const serviceAccount = require('../login.json');
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_CONFIG.replace(/\\\\n/g, "\\n") // Cambia \\n a \n
+);
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
